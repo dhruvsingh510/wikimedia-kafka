657598ea8fae49458229c2cb3d88e2efa453bd2f
dhruvsingh510 <dhruvsingh510@gmail.com>
2023-11-01 13:50:00 +0300

    adding kafka consumer

--- kafka-consumer-database/src/main/java/microservice/course/kafkaconsumerdatabase/KafkaConsumerDatabaseApplication.java
+++ kafka-consumer-database/src/main/java/microservice/course/kafkaconsumerdatabase/KafkaConsumerDatabaseApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class KafkaConsumerDatabaseApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(KafkaConsumerDatabaseApplication.class, args);
 	}
 
